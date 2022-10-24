16fef203bb65a994d7716d0d5abb9ac84000ee93
AlexantheR <adinu90@gmail.com>
2022-10-24 23:53:09 +0300

    Modified Tema2

--- Tema2/app/src/main/java/com/example/tema2/MainActivity.java
+++ Tema2/app/src/main/java/com/example/tema2/MainActivity.java
@@ -4,10 +4,8 @@ import static com.example.tema2.R.*;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.Image;
-import android.media.Rating;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,12 +14,11 @@ import android.widget.RatingBar;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.Switch;
-
-import java.text.SimpleDateFormat;
-import java.util.Locale;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int PICK_IMAGE = 1;
     private Movie movie;
     private EditText editTitle;
     private Spinner spGen;
@@ -51,26 +48,32 @@ public class MainActivity extends AppCompatActivity {
         ivPoster = findViewById(id.imgPlus);
         swSeek = findViewById(id.swSeek);
 
-//        EditText etTitlu = findViewById(id.editTextTitlu);
-//        EditText etDate = findViewById(id.editTextDate);
-//        RatingBar rtBar = findViewById(id.ratingBar);
-//        SeekBar skBudget = findViewById(id.seekBuget);
-//        ImageView pisica = findViewById(id.imgPisica);
-//        Switch swSeek = findViewById(id.swSeek);
-//
-//        Button btnSave = findViewById(id.btnSave);
-//        btnSave.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if(etTitlu.getText().toString().isEmpty())
-//                    etTitlu.setError("Introduceti titlul!");
-//                else
-//                    if(etDate.getText().toString().isEmpty())
-//                        etDate.setError("Introduceti data!");
-//                    else{
-//
-//                    }
-//            }
-//        });
+        Button btnSave = findViewById(id.btnSave);
+        btnSave.setOnClickListener(view -> {
+            movie=new Movie();
+            movie.setTitle(editTitle.getText().toString());
+            movie.setGen(Genre.valueOf(spGen.getSelectedItem().toString()));
+            movie.setRating((byte) rbMovie.getRating());
+            movie.setRelease(release.getText().toString());
+            movie.setBudget(sbBudget.getProgress());
+            movie.setSeekable(swSeek.isChecked());
+            Toast.makeText(MainActivity.this, "\n" +
+                    "Title: " + movie.getTitle() + "\n" +
+                    "Genre: " + movie.getGen() + "\n" +
+                    "Rating: " + movie.getRating() + "\n" +
+                    "Date: " + movie.getRelease() + "\n" +
+                    "Budget: " + movie.getBudget() + "\n" +
+                    "Seekable: " + movie.getSeekable(), Toast.LENGTH_LONG).show();
+        });
+
+
+            ImageView btnPick = findViewById(id.imgPlus);
+            btnPick.setOnClickListener(view -> {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+            });
+
     }
 }
