ee8c9c40ca97bc0ed23b6310b4f08f00949f056c
AlexantheR <adinu90@gmail.com>
2022-10-25 21:27:39 +0300

    Modified Tema1 again

--- Tema1/app/src/main/java/com/example/tema1/MainActivity.java
+++ Tema1/app/src/main/java/com/example/tema1/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.tema1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -20,21 +22,31 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView pisica=findViewById(R.id.pisica);
         pisica.setOnClickListener(view -> {
-                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
-                startActivity(intent);
-//            Intent intent = new Intent(Intent.ACTION_SEND);
-//            intent.setType("image/*");
-//            String title = getResources().getString(R.string.chooser_title);
-//            Intent chooser = Intent.createChooser(intent,title);
-//            startActivity(chooser);
+//                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+//                startActivity(intent);
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("image/*");
+            String title = getResources().getString(R.string.chooser_title);
+            Intent chooser = Intent.createChooser(intent,title);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(chooser);
+            }
         });
 
         ImageView caine=findViewById(R.id.caine);
         caine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), ThirdActivity.class);
-                startActivity(intent);
+//                Intent intent = new Intent(getApplicationContext(), ThirdActivity.class);
+//                startActivity(intent);
+
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("image/*");
+                String title = getResources().getString(R.string.chooser_title);
+                Intent chooser = Intent.createChooser(intent,title);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(chooser);
+                }
             }
         });
     }
